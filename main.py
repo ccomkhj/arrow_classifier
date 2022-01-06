2b7b05ac2300c7e895e2731eacccadead7b515ee
ccomkhj <ccomkhj@gmail.com>
2022-01-07 07:07:08 +0900

    skeleton update

--- main.py
+++ main.py
@@ -16,20 +16,27 @@ for image in images:
     # convert the image to grayscale format
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # apply binary thresholding
-    contours, hierarchy = cv2.findContours(image=img_gray, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+    # leave only arrow
+    ret,arrow = cv2.threshold(img_gray,80,255,cv2.THRESH_BINARY_INV)
+
+    # skeltonization
+    thinned = cv2.ximgproc.thinning(arrow.astype(np.uint8))
+
+    contours, hierarchy = cv2.findContours(image=thinned, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+
+    # keep only the main contour (ignore noise)
+    contour = max(contours, key=len)
+
+    # simplify the contour using Douglas
+    epsilon = 0.01*cv2.arcLength(contour,True)
+    approx = cv2.approxPolyDP(contour,epsilon,True)
 
     drawing = np.zeros(np.shape(img)[:-1])
-    cv2.drawContours(image=drawing, contours=contours, contourIdx=-1, color=255, thickness=2, lineType=cv2.LINE_AA)
+    cv2.drawContours(drawing,[approx],0,255,1)
+    print('next')
+    #TODO: check the point where the orientation changes, and compute the consine value between angles.
 
-    thinned = cv2.ximgproc.thinning(drawing)
-
-
-    cv2.imshow('Binary image', img_gray)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    cv2.imshow('Thined image', thinned)
+    cv2.imshow('skeleton arrow', drawing)
     cv2.waitKey(0)
 
     cv2.destroyAllWindows()
